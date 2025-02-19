c545de9891475bb83c6189bf3e2cf53c1030a5a3
Hasan Çavdar <hasan_cavdar2017@outlook.com>
2025-02-19 19:39:47 +0300

    09 completed

---
+++ 09_DatabaseProject/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DatabaseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Ado.net
+
+            Console.WriteLine("***** C# Ürün-Kategori Bilgi Sistemi ");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tablenumber;
+
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış ");
+            Console.Write("Getirmek istediğiniz tablo numarasını gir ");
+            tablenumber = Console.ReadLine();
+            Console.WriteLine("-----------------------------------");
+
+            SqlConnection connection = new SqlConnection("Data Source=hasan;Integrated Security=True;Trust Server Certificate=True");
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("Select * From TblCategory", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+
+            adapter.Fill(dataTable);
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString());
+                }
+                Console.WriteLine();
+            }
+
+
+
+            Console.Read();
+
+
+        }
+    }
+}
